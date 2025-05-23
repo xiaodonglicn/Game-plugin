6aa7103eb963aa5df2b6f829deb8282d645dfc88
xiaodonglicn <1158672373@qq.com>
2025-05-23 18:16:41 +0800

    game-plugin 优化2

--- src/main/java/com/lxd/tool/GameToolWindowFactory.java
+++ src/main/java/com/lxd/tool/GameToolWindowFactory.java
@@ -1,5 +1,6 @@
 package com.lxd.tool;
 
+import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.openapi.wm.ToolWindowFactory;
 import com.intellij.ui.content.Content;
@@ -7,10 +8,15 @@ import com.intellij.ui.content.ContentFactory;
 import org.jetbrains.annotations.NotNull;
 
 public class GameToolWindowFactory implements ToolWindowFactory {
+
+    private ContentFactory contentFactory;
+
     @Override
     public void createToolWindowContent(@NotNull com.intellij.openapi.project.Project project, @NotNull ToolWindow toolWindow) {
         GameToolWindow gameToolWindow = new GameToolWindow(toolWindow);
-        ContentFactory contentFactory = ContentFactory.SERVICE.getInstance();
+        if (contentFactory == null) {
+            contentFactory = ApplicationManager.getApplication().getService(ContentFactory.class);
+        }
         Content content = contentFactory.createContent(gameToolWindow.getContent(), "", false);
         toolWindow.getContentManager().addContent(content);
     }
